6cdef26e396fe531770b9a00955370e7914911fc
MD Touhedul Islam <110459773+ratul-prolific@users.noreply.github.com>
2025-10-29 06:58:18 +0600

    Watch course

--- src/components/pages/account/WatchCourse.jsx
+++ src/components/pages/account/WatchCourse.jsx
@@ -11,10 +11,12 @@ import axiosInstance from '../../../api/axios';
 const WatchCourse = () => {
     const params = useParams();
     const [course, setCourse] = useState({});
+    const [activeLesson, setActiveLesson] = useState({});
     const fetchCourse = () => {
-        axiosInstance.get(`/courses/${params.id}`)
+        axiosInstance.get(`/watch-course/${params.id}`)
             .then(response => {
-                setCourse(response.data);
+                setCourse(response.data.course);
+                setActiveLesson(response.data.activeLesson);
             })
             .catch(error => {
                 console.log(error);
@@ -24,100 +26,86 @@ const WatchCourse = () => {
     useEffect(() => {
         fetchCourse();
     }, [])
-return (
-    <>
-        <Layout>
-            <section className='section-5'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-md-8'>
-                            <div className='video'>
-                                <video width="100%" height="500" controls>
-                                    <source src="movie.mp4" type="video/mp4" />
-                                    <source src="movie.ogg" type="video/ogg" />
-                                    Your browser does not support the video tag.
-                                </video>
-                            </div>
-                            <div className='meta-content'>
-                                <div className='d-flex justify-content-between align-items-center border-bottom pb-2 mb-3 pt-1'>
-                                    <h3 className='pt-2'>Introduction</h3>
-                                    <div>
-                                        <a href="" className='btn btn-primary px-3'>
-                                            Mark as complete <IoMdCheckmarkCircleOutline size={20} /> </a>
-                                    </div>
+    return (
+        <>
+            <Layout>
+                <section className='section-5 mt-5'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-md-8'>
+                                <div className='video'>
+                                    <video width="100%" height="500" controls>
+                                        <source src="movie.mp4" type="video/mp4" />
+                                        <source src="movie.ogg" type="video/ogg" />
+                                        Your browser does not support the video tag.
+                                    </video>
                                 </div>
-                                <div>
-                                    <p>In this chapter we will learn about html basics</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='col-md-4'>
-                            <div className='card rounded-0'>
-                                <div className='card-body'>
-                                    <div className='h6'>
-                                        <strong>{course?.title}</strong>
-                                    </div>
-                                    <div className='py-2'>
-                                        <ProgressBar now={50} />
-                                        <div className='pt-2'>
-                                            50% complete
+                                <div className='meta-content'>
+                                    <div className='d-flex justify-content-between align-items-center border-bottom pb-2 mb-3 pt-1'>
+                                        <h3 className='pt-2'>
+                                            {
+                                                activeLesson && activeLesson.title
+                                            }
+                                        </h3>
+                                        <div>
+                                            <a href="" className='btn btn-primary px-3'>
+                                                Mark as complete <IoMdCheckmarkCircleOutline size={20} /> </a>
                                         </div>
                                     </div>
-                                    <Accordion defaultActiveKey="0" flush>
-                                        <Accordion.Item eventKey="0">
-                                            <Accordion.Header>Web Development basics</Accordion.Header>
-                                            <Accordion.Body className='pt-2 pb-0 ps-0'>
-                                                <ul className='lessons mb-0'>
-                                                    <li className='pb-2'>
-                                                        <a href="">
-                                                            <MdSlowMotionVideo size={20} /> Introduction
-                                                        </a>
-                                                    </li>
-                                                    <li className='pb-2'>
-                                                        <a href="">
-                                                            <MdSlowMotionVideo size={20} /> What is HTML
-                                                        </a>
-                                                    </li>
-                                                    <li className='pb-2'>
-                                                        <a href="">
-                                                            <MdSlowMotionVideo size={20} /> HTML Elements
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-                                        <Accordion.Item eventKey="1">
-                                            <Accordion.Header>HTML</Accordion.Header>
-                                            <Accordion.Body className='pt-2 pb-0 ps-0'>
-                                                <ul className='lessons mb-0'>
-                                                    <li className='pb-2'>
-                                                        <a href="">
-                                                            <MdSlowMotionVideo size={20} /> Introduction
-                                                        </a>
-                                                    </li>
-                                                    <li className='pb-2'>
-                                                        <a href="">
-                                                            <MdSlowMotionVideo size={20} /> What is HTML
-                                                        </a>
-                                                    </li>
-                                                    <li className='pb-2'>
-                                                        <a href="">
-                                                            <MdSlowMotionVideo size={20} /> HTML Elements
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-                                    </Accordion>
+                                    <div>
+                                        <p>In this chapter we will learn about html basics</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-md-4'>
+                                <div className='card rounded-0'>
+                                    <div className='card-body'>
+                                        <div className='h6'>
+                                            <strong>{course?.title}</strong>
+                                        </div>
+                                        <div className='py-2'>
+                                            <ProgressBar now={0} />
+                                            <div className='pt-2'>
+                                                0% complete
+                                            </div>
+                                        </div>
+                                        <Accordion defaultActiveKey="0" flush>
+                                            {
+                                                course.chapters && course.chapters.map(chapter => {
+
+                                                    return (
+                                                        <Accordion.Item eventKey="0">
+                                                            <Accordion.Header>{chapter.title}</Accordion.Header>
+                                                            <Accordion.Body className='pt-2 pb-0 ps-0'>
+                                                                <ul className='lessons mb-0'>
+                                                                    {
+                                                                        chapter.lessons && chapter.lessons.map(lesson => {
+                                                                            return (
+                                                                                <li className='pb-2'>
+                                                                                    <a href="">
+                                                                                        <MdSlowMotionVideo size={20} /> {lesson.title}
+                                                                                    </a>
+                                                                                </li>
+                                                                            )
+                                                                        })
+                                                                    }
+                                                                </ul>
+                                                            </Accordion.Body>
+                                                        </Accordion.Item>
+                                                    )
+                                                }
+                                                )
+                                            }
+                                        </Accordion>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </Layout>
-    </>
-)
+                </section>
+            </Layout>
+        </>
+    )
 }
 
 export default WatchCourse
